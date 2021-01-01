64c582eb26dbd7bf77cad20b0007d771d5cbee14
chayeoi <chayeoikeem@gmail.com>
2021-01-01 14:24:53 +0900

    feat: framer-motion-tutorial 추가
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

---
+++ framer-motion-tutorial/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'simple-import-sort',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'multiline-ternary': 'off',
+    'node/no-path-concat': 'off',
+    'no-use-before-define': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'simple-import-sort/imports': ['error', {
+      groups: [
+        ['^react', '^@?\\w'],
+        ['^@?\\w'],
+        ['^'],
+        ['^\\.'],
+        ['^\\u0000'],
+      ],
+    }],
+    'simple-import-sort/exports': 'error',
+    'space-before-function-paren': 'off',
+  },
+}
---
+++ framer-motion-tutorial/README.md
@@ -0,0 +1,5 @@
+# Framer Motion Tutorial
+
+## Resources
+
+- [Framer Motion (for React) | The Net Ninja - YouTube](https://www.youtube.com/watch?v=2V1WK-3HQNk&list=PL4cUxeGkcC9iHDnQfTHEVVceOEBsOf07i)
